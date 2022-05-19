b6f14762fac194a5b8841e0d529f40dfc90f6d10
98lxh <582658211@qq.com>
2022-05-20 07:50:46 +0800

    feat:async request method

---
+++ src/utils/http.ts
@@ -0,0 +1,48 @@
+import qs from "qs"
+import * as auth from "auth-provider"
+const apiUrl = process.env.REACT_APP_API_URL
+
+interface Config extends RequestInit {
+  token: string,
+  data: object
+}
+
+export const http = async (endpoint: string, { data, token, headers, ...customConfig }: Config) => {
+  const config = {
+    method: 'GET',
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '',
+      'Content-Type': data ? 'application/json' : ''
+    },
+    ...customConfig
+  }
+
+  if (config.method.toUpperCase() === 'GET') {
+    endpoint += `?${qs.stringify(data)}`
+  } else {
+    config.body = JSON.stringify(data || {})
+  }
+
+  return window.fetch(`${apiUrl}/${endpoint}`, config)
+    .then(async response => {
+      const { status } = response
+
+      switch (status) {
+        case 401:
+          //登录状态过期
+          auth.logout()
+          window.location.reload()
+          return Promise.reject({ message: '请重新登录' })
+      }
+
+      const data = await response.json()
+
+      if (response.ok) {
+        return data
+      } else {
+        return Promise.reject(data)
+      }
+    })
+}
+
+
