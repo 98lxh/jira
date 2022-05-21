9e057c7ffa0e448ae9589b7a04d5262f39eb9ce9
98lxh <582658211@qq.com>
2022-05-21 09:38:58 +0800

    feat:use grid layout

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,11 +1,43 @@
+import React from "react"
 import { useAuth } from "context/auth-context"
-import React, { memo } from "react"
 import { ProjectListScreen } from "screens/project-list"
-export const AuthenticatedApp: React.FC = memo(() => {
+import styled from "@emotion/styled"
+import { Row } from "components/lib"
+
+export const AuthenticatedApp: React.FC = () => {
   const { logout } = useAuth()
 
-  return <div>
-    <button onClick={logout}>登出</button>
-    <ProjectListScreen />
-  </div>
-})
+  return (
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h3>logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight onClick={logout}>登出</HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
+  )
+}
+
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+`;
+
+const Header = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`;
+
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.div``;
